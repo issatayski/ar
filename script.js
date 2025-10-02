19edc079dabb300e3a48896c9a340f330aae4141
Исатай <67948442+issatayski@users.noreply.github.com>
2025-10-02 21:06:02 +0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const viewer = document.getElementById("viewer");
-  // страховка: атрибуты для AR
-  viewer.setAttribute("ar-placement", "floor");
-  viewer.setAttribute("ar-scale", "fixed");
-});
